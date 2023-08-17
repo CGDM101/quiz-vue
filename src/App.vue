08263861f3593563ad71579a7c0c00f27589c744
Camilla Gustafsson <camilla.gustafsson@jellyhive.se>
2023-08-17 13:54:32 +0200

    Added all necessary html elements.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
 </script>
 
-<template></template>
+<template>
+    <header>Depeche Mode Lyrics Quiz</header>
+    <main>
+        <p>du har x poäng av x poäng</p>
+        <p>Frågan</p>
+        <div>
+            <button>opt1</button>
+            <button>opt2</button>
+            <button>opt2</button>
+            <button>opt3</button>
+        </div>
+    </main>
+</template>
 
 <style scoped></style>
